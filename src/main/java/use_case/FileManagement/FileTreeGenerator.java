6af04599c0bddfaff45830f7fe1ab489594607d6
dariosanp <dario.sanchezpuentes@mail.utoronto.ca>
2024-11-08 17:35:46 -0500

    FileTree createFile() finished

--- src/main/java/use_case/FileManagement/FileTreeGenerator.java
+++ src/main/java/use_case/FileManagement/FileTreeGenerator.java
@@ -2,7 +2,9 @@ package use_case.FileManagement;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 /**
@@ -15,6 +17,10 @@ public class FileTreeGenerator {
     //TODO: check if this actually works. It automatically generates JTree from chosen direcotry structure...
     private JTree fileTree;
     private JScrollPane fileScrollPane;
+    //made root an instance variable so we can update the tree.
+    private DefaultMutableTreeNode rootNode;
+    //made the directory an instance variable to recycle it in create, delete, and save.
+    private File directory;
 
     public FileTreeGenerator() {
         chooseDiretory();
@@ -26,8 +32,9 @@ public class FileTreeGenerator {
         int option = fileChooser.showOpenDialog(null);
 
         if (option == JFileChooser.APPROVE_OPTION) {
-            File selectedDir = fileChooser.getSelectedFile();
-            DefaultMutableTreeNode rootNode = createNodesFromDirectory(selectedDir);
+            //refactored to the instance variable.
+            directory = fileChooser.getSelectedFile();
+            rootNode = createNodesFromDirectory(directory);
             fileTree = new JTree(rootNode);
             fileScrollPane = new JScrollPane(fileTree);
         }
@@ -49,12 +56,55 @@ public class FileTreeGenerator {
         return rootNode;
     }
 
+    public File getDirectory() {
+        return directory;
+    }
+
     public JScrollPane getFileScrollPane() {
         return fileScrollPane;
     }
 
-    public void createFile(File parentDirectory) {
-        //TODO
+    public void createFile(File parent) {
+        String fileName = JOptionPane.showInputDialog(null, "File name:", "File", JOptionPane.QUESTION_MESSAGE);
+
+        if (fileName == null || fileName.trim().isEmpty()) {
+            return;
+        }
+
+        File newFile = new File(parent, fileName);
+        try {
+            if (newFile.createNewFile()) {
+                DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(newFile.getName());
+                DefaultMutableTreeNode parentNode = findNodeByName(parent.getName(), rootNode);
+                if (parentNode != null) {
+                    parentNode.add(newNode);
+                    ((DefaultTreeModel) fileTree.getModel()).reload(parentNode);
+                } else {
+                    System.out.println("File not found");
+                    return;
+                }
+
+            } else {
+                JOptionPane.showMessageDialog(null, "File already exists", "File", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "File could not be created", "File", JOptionPane.ERROR_MESSAGE);
+        } catch (SecurityException e) {
+            JOptionPane.showMessageDialog(null, "Security access denied", "File", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    private DefaultMutableTreeNode findNodeByName(String nodeName, DefaultMutableTreeNode root) {
+        if (root.toString().equals(nodeName)) {
+            return root;
+        }
+        for (int i = 0; i < root.getChildCount(); i++) {
+            DefaultMutableTreeNode child = (DefaultMutableTreeNode) root.getChildAt(i);
+            DefaultMutableTreeNode result = findNodeByName(nodeName, child);
+            if (result != null) {
+                return result;
+            }
+        }
+        return null;
     }
 
     public void deleteFile(File parentDirectory) {
