9ad67f636831fe3bdc5d608b509bcf8379c75122
marburcea <maria.burcea@mail.utoronto.ca>
2024-11-08 10:47:35 -0500

    added code to AutoCompleteOperations

--- src/main/java/use_case/AutoCompleteOperations/AutoCompleteOperations.java
+++ src/main/java/use_case/AutoCompleteOperations/AutoCompleteOperations.java
@@ -6,4 +6,16 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class AutoCompleteOperations {
+
+    public void AutoCompleteItem(JTextComponent textComponent, JPopupMenu popupMenu) {
+        // use json file or string?
+        textComponent.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                String text = textComponent.getText();
+                //check lastword getter
+                String lastword = text.substring(text.lastIndexOf(" ") + 1);
+                if (e.getKeyChar() == KeyEvent.VK_ENTER) {}
+            }
+        });
+    }
 }
