c2609ad62b8f2ad1d766d6fa8f167d5da8de7646
AriGood <68861355+AriGood@users.noreply.github.com>
2024-12-01 15:54:37 -0500

    fixed some issues in GitMenuBuilder mostly having to do with error handling

--- src/main/java/entity/CredentialEncryption.java
+++ src/main/java/entity/CredentialEncryption.java
@@ -1,10 +1,12 @@
 package entity;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
+import javax.crypto.*;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -35,13 +37,17 @@ public class CredentialEncryption {
      * @param password the password to derive the key from.
      * @param salt     the salt to use in the key derivation.
      * @return a SecretKey for AES encryption/decryption.
-     * @throws Exception if the key derivation process fails.
+     * @throws EncryptionException if the key derivation process fails.
      */
-    public static SecretKey deriveKey(char[] password, byte[] salt) throws Exception {
-        PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        byte[] keyBytes = factory.generateSecret(spec).getEncoded();
-        return new SecretKeySpec(keyBytes, "AES");
+    public static SecretKey deriveKey(char[] password, byte[] salt) throws EncryptionException {
+        try {
+            PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            byte[] keyBytes = factory.generateSecret(spec).getEncoded();
+            return new SecretKeySpec(keyBytes, "AES");
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new EncryptionException(e);
+        }
     }
 
     /**
@@ -50,12 +56,17 @@ public class CredentialEncryption {
      * @param data the plaintext string to encrypt.
      * @param key  the AES key to use for encryption.
      * @return a byte array containing the encrypted data.
-     * @throws Exception if the encryption process fails.
+     * @throws EncryptionException if the encryption process fails.
      */
-    public static byte[] encrypt(String data, SecretKey key) throws Exception {
-        javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance("AES");
-        cipher.init(javax.crypto.Cipher.ENCRYPT_MODE, key);
-        return cipher.doFinal(data.getBytes());
+    public static byte[] encrypt(String data, SecretKey key) throws EncryptionException {
+        try {
+            javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance("AES");
+            cipher.init(javax.crypto.Cipher.ENCRYPT_MODE, key);
+            return cipher.doFinal(data.getBytes());
+        } catch (NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | InvalidKeyException |
+                 BadPaddingException e) {
+            throw new EncryptionException(e);
+        }
     }
 
     /**
@@ -64,12 +75,16 @@ public class CredentialEncryption {
      * @param data the encrypted byte array to decrypt.
      * @param key  the AES key to use for decryption.
      * @return the decrypted plaintext string.
-     * @throws Exception if the decryption process fails.
+     * @throws EncryptionException if the decryption process fails.
      */
-    public static String decrypt(byte[] data, SecretKey key) throws Exception {
-        javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance("AES");
-        cipher.init(javax.crypto.Cipher.DECRYPT_MODE, key);
-        return new String(cipher.doFinal(data));
+    public static String decrypt(byte[] data, SecretKey key) throws EncryptionException {
+        try{
+            javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance("AES");
+            cipher.init(javax.crypto.Cipher.DECRYPT_MODE, key);
+            return new String(cipher.doFinal(data));
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | InvalidKeyException | BadPaddingException e) {
+            throw new EncryptionException(e);
+        }
     }
 
     /**
@@ -90,5 +105,11 @@ public class CredentialEncryption {
      */
     public static byte[] decodeBase64(String data) {
         return Base64.getDecoder().decode(data);
+    }
+
+    public static class EncryptionException extends Exception{
+        EncryptionException(Exception ex) {
+            super(ex);
+        }
     }
 }
